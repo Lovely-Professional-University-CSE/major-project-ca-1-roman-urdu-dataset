e69d8d051ab9f3e13602bc6963cef52b39133564
Subhadip Mondal <msubhadip05@gmail.com>
2019-10-20 01:43:02 +0530

    Importing all necessary modules
     we have imported everything that we will be using for given task that is sentiment analysis on roman urdu

--- RUnlp.py
+++ RUnlp.py
@@ -1,1 +1,18 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.utils import shuffle
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+# making corpus or words from comments
+import re
+from nltk.stem.porter import PorterStemmer
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+
 
