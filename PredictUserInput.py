1a95fd0fa96e93413c69f9ba7af56c7a84b1ccb7
Mohammad Abuzar <56014364+atzabuzar@users.noreply.github.com>
2019-10-31 22:42:17 +0530

    import classifier
    imported classifier using load pickel module and created gui window for better visualization

--- PredictUserInput.py
+++ PredictUserInput.py
@@ -5,3 +5,15 @@ user_input_window = Tk()
 #from MyModel import clean, View_ConfusionMatrix, Visualize_ActualDataSet
 import pickle
 import MyModel
+
+
+clf_classifier = pickle.load(open( "classifier.sav", "rb" ))
+
+user_input_window.title("Predict User Input Comment")
+#user_input_window.wm_attributes("-fullscreen", "true")
+user_input_window.config(bg="cyan")
+user_input_window.iconbitmap("img/lpu_logo.ico")
+
+PositiveImage = PhotoImage(file = 'img/positive.gif')
+NegativeImage = PhotoImage(file = 'img/negative.gif')
+NeutralImage = PhotoImage(file = 'img/neutral.gif')
