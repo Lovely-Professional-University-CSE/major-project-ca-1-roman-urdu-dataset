0c6e3d82374f915a761cc5dbb2c1752d1d0f273f
Mohammad Abuzar <56014364+atzabuzar@users.noreply.github.com>
2019-10-31 22:49:20 +0530

    Update PredictUserInput.py

--- PredictUserInput.py
+++ PredictUserInput.py
@@ -17,3 +17,18 @@ user_input_window.iconbitmap("img/lpu_logo.ico")
 PositiveImage = PhotoImage(file = 'img/positive.gif')
 NegativeImage = PhotoImage(file = 'img/negative.gif')
 NeutralImage = PhotoImage(file = 'img/neutral.gif')
+
+
+def Predict_User_Input():
+	user_input = Input_String.get()
+	user_input = MyModel.clean(user_input)
+	user_input = [user_input]
+	user_input_vector = current_vector.transform(user_input)
+	Sentiment_user_input = clf_classifier.predict(user_input_vector)
+	#Label(user_input_window, text = Sentiment_user_input).grid(row = 2, columnspan = 2)
+	if Sentiment_user_input == 'Positive':
+		Label(user_input_window, image=PositiveImage).grid(row = 2)
+	elif Sentiment_user_input == 'Negative':
+		Label(user_input_window, image=NegativeImage).grid(row = 2)
+	else:
+		Label(user_input_window, image=NeutralImage).grid(row = 2)
