1ae054bfa0412fb4932d054e174136052c5c0c63
Subhadip Mondal <msubhadip05@gmail.com>
2019-11-06 19:45:02 +0530

    created About_Dev() function
    created About_Dev() function to know about the developer

--- home.py
+++ home.py
@@ -1,2 +1,31 @@
 from tkinter import *
-import sys
+import os
+home_window = Tk()
+home_window.title("Home")
+home_window.wm_attributes("-fullscreen", "true")
+home_window.config(bg="cyan")
+home_window.iconbitmap("img/lpu_logo.ico")
+
+sentiment1 = PhotoImage(file = 'img/sentiment_home.gif')
+about_us_dev = PhotoImage(file = 'img/about_developers.gif')
+take_UI = PhotoImage(file = 'img/user_input.gif')
+
+Label(home_window, image = sentiment1).grid(row = 0, column = 0, columnspan = 2, padx = 100, pady = 20)
+
+def exit_window():
+	home_window.destroy()
+exit_image = PhotoImage(file = 'img/exit_image.gif')
+Button(home_window, image=exit_image, cursor="hand2", command = exit_window).grid(row = 0, column = 2)
+
+
+def About_Dev():
+	home_window.destroy()
+	os.system('python about_us.py')
+Button(home_window, image = about_us_dev, font="halston 20 italic", cursor="hand2", command = About_Dev).grid(row = 1, column = 0)
+
+def Predict_Input():
+	home_window.destroy()
+	os.system('python PredictUserInput.py')
+Button(home_window,  image = take_UI, font="halston 20 italic", cursor="hand2", command = Predict_Input).grid(row = 1, column = 1)
+
+home_window.mainloop()
