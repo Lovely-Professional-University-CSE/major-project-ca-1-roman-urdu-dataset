05a36bc783830070d2a00fc85a85f3a2948de492
Subhadip Mondal <msubhadip05@gmail.com>
2019-11-11 00:24:04 +0530

    Updating cleaning function

--- RUnlp.py
+++ RUnlp.py
@@ -1,24 +1,30 @@
-# Importing the libraries
+'''
+Step 1: Importing all necessary modules
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.utils import shuffle
-from sklearn.preprocessing import LabelEncoder,OneHotEncoder
-# making corpus or words from comments
 import re
-from nltk.stem.porter import PorterStemmer
-import nltk
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from nltk import word_tokenize
 
-dataset = pd.read_csv('F:\Sentiment Analysis on Roman Urdu Dataset\Roman Urdu DataSet.csv', names=['Comment', 'sentiment', 'nan'])
+print('Wait Model is learning...')
 
-print(dataset.head())
-print(dataset.shape)
+'''
+Step 2: Import Dataset
+'''
+dataset = pd.read_csv('Roman Urdu DataSet.csv', names=['comment', 'sentiment', 'nan'])
+
+'''
+Step 3: Lets have a look at our data set
+'''
 
 Pos = dataset[dataset['sentiment'] == 'Positive'].shape[0]
 Neg = dataset[dataset['sentiment'] == 'Negative'].shape[0]
 Neu = dataset[dataset['sentiment'] == 'Neutral'].shape[0]
-# bar plot of the 3 classes
 plt.bar(10,Pos,3, label="Positve")
 plt.bar(15,Neg,3, label="Negative")
 plt.bar(20,Neu,3, label="Neutral")
@@ -27,31 +33,26 @@ plt.ylabel('Number of examples')
 plt.title('Proportion of examples')
 plt.show()
 
-# label selection
-y=dataset.iloc[:,1].values
-labelEnocder_y=LabelEncoder()
-y=labelEnocder_y.fit_transform(y)
-# 3 postive 1 negative 2 nuetral
+'''
+Step 4: We have y in form of categorical data
+'''
+y = dataset['sentiment']
 
-print(y)
-print(y.shape)
 
-corpus=[]
+'''
+Step 5: Cleaning
+'''
+# created user defined stopwords
 stopwords=['ai', 'ayi', 'hy', 'hai', 'main', 'ki', 'tha', 'koi', 'ko', 'sy', 'woh', 'bhi', 'aur', 'wo', 'yeh', 'rha', 'hota', 'ho', 'ga', 'ka', 'le', 'lye', 'kr', 'kar', 'lye', 'liye', 'hotay', 'waisay', 'gya', 'gaya', 'kch', 'ab', 'thy', 'thay', 'houn', 'hain', 'han', 'to', 'is', 'hi', 'jo', 'kya', 'thi', 'se', 'pe', 'phr', 'wala', 'waisay', 'us', 'na', 'ny', 'hun', 'rha', 'raha', 'ja', 'rahay', 'abi', 'uski', 'ne', 'haan', 'acha', 'nai', 'sent', 'photo', 'you', 'kafi', 'gai', 'rhy', 'kuch', 'jata', 'aye', 'ya', 'dono', 'hoa', 'aese', 'de', 'wohi', 'jati', 'jb', 'krta', 'lg', 'rahi', 'hui', 'karna', 'krna', 'gi', 'hova', 'yehi', 'jana', 'jye', 'chal', 'mil', 'tu', 'hum', 'par', 'hay', 'kis', 'sb', 'gy', 'dain', 'krny', 'tou']
-for i in range(0,len(y)):
-    review = re.sub('[^a-zA-Z]',' ',str(dataset.iloc[:,0].values[i]))
-    review=review.lower()
-    review=review.split()
-    review=[word for word in review if not word in stopwords]
-    review=' '.join(review)
-    corpus.append(review)
-stopwords, len(stopwords)
 
-corpus
+def clean(x):
+    review_with_no_special_character = re.sub('[^a-zA-Z]',' ',str(x))
+    review_in_lowercase = review_with_no_special_character.lower()
+    review_in_tokens = word_tokenize(review_in_lowercase)
+    review_with_no_stopwords = [word for word in review_in_tokens if not word in stopwords]
+    review_in_sentence = ' '.join(review_with_no_stopwords)
+    return review_in_sentence
 
-cv=CountVectorizer(max_features=2500)
-x=cv.fit_transform(corpus).toarray()
+dataset['comment'] = dataset['comment'].apply(lambda x:clean(x))
 
-cv
-
-x, x.shape
+X = dataset['comment']
